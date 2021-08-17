ddb04c71db71cccae07ec9979b0bea598b7054fc
Babatunde Ojo <babatundeojo@MacBook-Pro.local>
2021-08-17 11:04:49 +0100

    add login view

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import AppHeader from '../components/app-bar';
 import Login from './login';
 
 const Screens = () => (
   <>
-    <AppHeader />
     <Router>
       <Route exact path="/" component={Login} />
       <Route exact path="/login" component={Login} />
